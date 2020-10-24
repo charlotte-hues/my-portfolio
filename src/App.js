c943f3421e35368f4349301d408009e211f5a69c
charlotte-hues <hues.charlotte@gmail.com>
2020-10-24 19:38:29 +0100

    Added threeJs cubes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
+import Header from "./shared/components/header";
 
-function App() {
-  return <div className="App">Hues Basic Template Test</div>;
-}
+import { Canvas, useFrame } from "react-three-fiber";
+import { softShadows } from "drei";
+
+softShadows();
+
+const SpinningCube = ({ position, color, args }) => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh castShadow ref={mesh} position={position}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial attach="material" color={color} />
+    </mesh>
+  );
+};
+
+const Lighting = () => {
+  return (
+    <React.Fragment>
+      <ambientLight intensity={0.3} />
+      <directionalLight intensity={0.2} castShadow />
+      <pointLight intensity={0.5} position={[-10, 0, -20]} />
+      <pointLight intensity={1.5} position={[0, -10, 0]} />
+    </React.Fragment>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
+        <Lighting />
+
+        <SpinningCube args={[2, 2, 1]} position={[2, 0, 0]} color="grey" />
+        <SpinningCube args={[1, 1, 2]} position={[-2, 1, 0]} color="orange" />
+        <SpinningCube args={[1, 1, 0.1]} position={[0, 0, 0]} color="aqua" />
+
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.05} />
+          </mesh>
+        </group>
+      </Canvas>
+    </>
+  );
+};
 
 export default App;
---
+++ src/shared/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="navText">
+        <span>REACT THREE FIBER</span>
+      </div>
+      <div className="navText">
+        <span>EP. 1</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
